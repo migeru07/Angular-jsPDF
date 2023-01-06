439d3ec16b77079502280f44acbc014b3932013f
migeru07 <34461583+migeru07@users.noreply.github.com>
2023-01-06 16:01:51 -0400

    Including pdfmake

--- src/app/components/pdfmake/pdfmake.component.ts
+++ src/app/components/pdfmake/pdfmake.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import * as jsPDF from 'jspdf';
+//import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-pdfmake',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PdfmakeComponent implements OnInit {
 
+  //@ViewChild('downloadButton') downloadButton: ElementRef = new ElementRef(null);
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+  // ngAfterViewInit() {
+  //   this.downloadButton.nativeElement.addEventListener('click', () => {
+  //     // Genera el PDF y lo descarga al presionar el botón
+  //     const doc = new jsPDF();
+  //     doc.text('Mi título', 10, 10);
+  //     doc.text('Mi contenido', 10, 20);
+  //     doc.save('mi_archivo.pdf');
+  //     FileSaver.saveAs(doc.output('blob'), 'mi_archivo.pdf');
+  //   });
+  // }
 
 }
